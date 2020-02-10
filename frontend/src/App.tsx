cdeac69fd0a969156d943c9da8e5a2b0c2526836
Pedro Guilherme <pedro.schmid2311@gmail.com>
2020-02-10 05:21:44 -0300

    Comit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { TextField } from './components/TextField';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <TextField text='hello'/>
+    </div>
+  )
+}
+
+export default App;
